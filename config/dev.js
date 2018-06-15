699341a91dc41560272765c4161c4edce74adc63
Roy Kim <rykim79@gmail.com>
2018-06-15 09:57:03 -0400

    first commit, login auth complete

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: '845211628234-mhmdvf79ashfl4gu04n8np1v6vd5iehl.apps.googleusercontent.com',
+  googleClientSecret: 'MtPsMzZAXMdFtMjWcusEnGzy',
+  mongoURI: 'mongodb://localhost/feedback-loop',
+  cookieKey: 'somerandomthing'
+
+}
